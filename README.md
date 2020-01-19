b284d09bd0e24616f6b122cdb3d86f57296f91af
TN Roebuck <tnroebuck@TNs-MacBook-Pro.local>
2020-01-18 18:16:57 -0700

    achieved MVP- both sides are functional

--- README.md
+++ README.md
@@ -54,15 +54,15 @@ const [value, setValue] = useState(); // Give these better names, and decide whe
 
 - [X] The scores in the JSX are currently hardcoded to 32 points each. Remove the hardcoded values
 - [X] Render the state values from what we just set up in steps 1 and 2
-- [ ] Play around with different initial values to test if they are rendering on the DOM correctly
+- [X] Play around with different initial values to test if they are rendering on the DOM correctly
 
 ### STEP 4 - Add Click Functionality to Increment the Scores
 
-- [ ] Add the `onClick` handler to each function
-- [ ] Determine how much you will need to increment the score for each button
-  - [ ] A touchdown is worth 7 points (assume the following extra point is made)
-  - [ ] A field goal is worth 3 points
-- [ ] Inside the click handlers on each button, use the setter functions for each team to increment the appropriate team's score by the correct amount.
+- [X] Add the `onClick` handler to each function
+- [X] Determine how much you will need to increment the score for each button
+  - [X] A touchdown is worth 7 points (assume the following extra point is made)
+  - [X] A field goal is worth 3 points
+- [X] Inside the click handlers on each button, use the setter functions for each team to increment the appropriate team's score by the correct amount.
 
 ## Stretch Problems
 
--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [lionsScore, setLionsScore] = useState(7);
-  const [tigersScore, setTigersScore] = useState(3);
+  const [lionsScore, setLionsScore] = useState(0);
+  const [tigersScore, setTigersScore] = useState(0);
 
   const addLionsTouchdown = event => {
     setLionsScore(lionsScore + 7);
@@ -44,12 +44,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick= {addLionsTouchdown} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick= {addLionsFieldgoal} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick= {addTigersTouchdown} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick= {addTigersFieldgoal} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
